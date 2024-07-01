08b6be8bb1ccfc2182c8526150c8b4b8e026e6d7
prajakta16m <prajakta.mahamuni91@gmail.com>
2024-07-01 16:24:50 +0530

    cart item number updates using ngmodel

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -3,11 +3,12 @@ import { CartService } from '../../services/cart.service';
 import { Product } from '../../data/product';
 import { CommonModule } from '@angular/common';
 import { CartItem } from '../../data/cartItem';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
@@ -28,16 +29,23 @@ export class CartComponent implements OnInit {
       this.setCartPrice();
   }
 
-  onInputChange(item: CartItem) {
-   
+  onInputChange(item: CartItem, _event: any) {
+    this.setCartPrice();
+    if(_event.target.value == 0){
+      this.removeItem(item.product);
+    }
   }
 
   setCartPrice() {
-    this.cartList.forEach(prod => 
+
+    if(this.cartList.length > 0) {
+      this.cartList.forEach(prod => 
         prod.totalPrice = prod.count * prod.price
       );
       let val = this.cartList.map(prod => prod.totalPrice).reduce((a,b) => a+b);
       this.totalCartPrice.set(Math.round(val * 100) / 100);
+    }
+    
   }
 
   ngOnInit(): void {
